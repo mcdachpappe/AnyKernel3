def57f34f7d7e72b2cf602b16d67a5332d741ced
mcdachpappe <noreference@web.de>
2025-07-23 13:57:36 +0200

    anykernel: Support future security patch levels
    Signed-off-by: mcdachpappe <noreference@web.de>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=mcd Kernel for the Pixel 9/Pro/Fold/XL
+kernel.string=mcd Kernel for Pixel 9 Series (Android 15)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=komodo
 device.name3=tokay
 device.name4=comet
 supported.versions=15
-supported.patchlevels=2024-12 - 2025-05
+supported.patchlevels=2024-12 -
 supported.vendorpatchlevels=
 '; } # end properties
 
